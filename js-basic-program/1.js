8c1c36d884be140238b02e0f7527763b985ac5dc
parthsapariya745 <parthsapariyait7@gmail.com>
2025-08-10 17:44:51 +0530

    update

--- js-basic-program/1.js
+++ js-basic-program/1.js
@@ -1,11 +1,14 @@
-let box = [10, 20, 30, 40, 50]
+let passwordLength = Math.floor(Math.random() * 13); 
+let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*+-<>?/";
+let password = "";
 
-for (let i = 0; i < box.length; i++) {
-    console.log(box[i])    
+if (passwordLength < 7) {
+    passwordLength += 7;
 }
-console.log("--")
-box.push(100)
 
-for (let i = 0; i < box.length; i++) {
-    console.log(box[i])    
+for (let i = 0; i < passwordLength; i++) {
+    let randomValue = Math.floor(Math.random() * characters.length);
+    password += characters[randomValue];
 }
+
+console.log(`Password: ${password}`);
